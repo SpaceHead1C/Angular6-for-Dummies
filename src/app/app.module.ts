a564abf822f5e755dd7e137d488ccdc85835c967
SpaceHead1C <damh17@gmail.com>
2018-12-13 16:11:35 +0300

    add component with command "ng g c <name>"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRootSecondComponent } from './app-root-second/app-root-second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRootSecondComponent
   ],
   imports: [
     BrowserModule,
