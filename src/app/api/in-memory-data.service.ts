46adfd8fb94b0649d318f1b659ada5857d225f51
SpaceHead1C <damh17@gmail.com>
2018-12-14 10:35:24 +0300

    api-service returns collection of users (http-path /api/users)

--- src/app/api/in-memory-data.service.ts
+++ src/app/api/in-memory-data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  /* This automaticly gives api with path "/api/users"
+   * If we add const "userGroups" for example then we "return { users, userGroups }"
+   */
+  createDb() {
+    const users = [
+      {firstname: 'Пётр', lastname: 'Петров'},
+      {firstname: 'Иван', lastname: 'Иванов'},
+      {firstname: 'Степан', lastname: 'Сидоров'}
+    ];
+    return { users };
+  }
 
   constructor() { }
 }
