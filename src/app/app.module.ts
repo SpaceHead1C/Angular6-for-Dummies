3f8ff1a62256e7957dbb5903ced7d778dc9288fd
SpaceHead1C <damh17@gmail.com>
2018-12-14 12:40:58 +0300

    add parameters "InMemoryDataService" (include al endpoints) and "dataEncapsulation" in method "forRoot(...)"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserTableComponent } from './user-table/user-table.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { InMemoryDataService } from './api/in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { UserListComponent } from './user-list/user-list.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
