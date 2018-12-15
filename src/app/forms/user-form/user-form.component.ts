b293ebb415aff6c0f5eb26542e9d7355d470f45c
SpaceHead1C <damh17@gmail.com>
2018-12-15 17:48:25 +0300

    two-way data binding

--- src/app/forms/user-form/user-form.component.ts
+++ src/app/forms/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  public user: User;
 
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      id: 1,
+      firstname: 'Кот',
+      lastname: 'Матроскин'
+    }
   }
 
 }
